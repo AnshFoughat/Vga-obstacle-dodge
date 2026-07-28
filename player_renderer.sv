7abb51489dde7a5022b5c00dc5ef6d2255b6ff3f
Ansh Foughat <158195999+AnshFoughat@users.noreply.github.com>
2026-07-28 13:19:15 +0530

    Update player_renderer.sv

--- player_renderer.sv
+++ player_renderer.sv
@@ -21,21 +21,12 @@
 
 
 module player_renderer(
-    input wire [9:0] pixel_x,
-    input wire [9:0] pixel_y,
-    input wire [9:0] player_y,
+    input [9:0] pixel_x,
+    input [9:0] pixel_y,
+    input [9:0] player_y,
 
-    output wire player_on
+    output player_on
 );
 
-localparam PLAYER_X = 100;
-localparam PLAYER_W = 50;
-localparam PLAYER_H = 50;
-
-assign player_on =
-       (pixel_x >= PLAYER_X) &&
-       (pixel_x <  PLAYER_X + PLAYER_W) &&
-       (pixel_y >= player_y) &&
-       (pixel_y <  player_y + PLAYER_H);
 
 endmodule
