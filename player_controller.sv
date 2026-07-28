373b7ca289792f655827b602b5098e4cb1401103
Ansh Foughat <158195999+AnshFoughat@users.noreply.github.com>
2026-07-28 13:18:44 +0530

    Refactor player_controller module inputs and logic
    Removed input wire declarations and simplified always block.

--- player_controller.sv
+++ player_controller.sv
@@ -21,25 +21,10 @@
 
 
 module player_controller(
-    input wire clk,
-    input wire rst,
-    input wire btn_up,
-    input wire btn_down,
-
+    input clk, rst,btn_up, btn_down,
     output reg [9:0] player_y
 );
 
-always @(posedge clk or posedge rst) begin
-    if(rst)
-        player_y <= 180;
-    else begin
 
-        if(btn_up && player_y > 5)
-            player_y <= player_y - 20;
-
-        else if(btn_down && player_y < 180)
-            player_y <= player_y + 20;
-    end
-end
 
 endmodule
