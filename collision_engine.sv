1e7f3ffd61788a2353757c82b05fae87f1a6a1dd
Ansh Foughat <158195999+AnshFoughat@users.noreply.github.com>
2026-07-28 13:12:38 +0530

    Refactor collision detection by removing parameters
    Removed parameters and collision detection logic for obstacles.

--- collision_engine.sv
+++ collision_engine.sv
@@ -32,40 +32,7 @@ module collision_engine(
     output wire collision
 );
 
-parameter PLAYER_X    = 100;
-parameter PLAYER_W    = 50;
-parameter PLAYER_H    = 50;
-
-parameter OBS_WIDTH   = 20;
-parameter OBS_HEIGHT  = 20;
-
-parameter GROUND_Y    = 230;
-
-wire c0,c1,c2,c3;
-
-assign c0 =
-    (PLAYER_X < obs0_x + OBS_WIDTH) &&
-    (PLAYER_X + PLAYER_W > obs0_x) &&
-    (player_y < GROUND_Y) &&
-    (player_y + PLAYER_H > GROUND_Y-OBS_HEIGHT);
-
-assign c1 =
-    (PLAYER_X < obs1_x + OBS_WIDTH) &&
-    (PLAYER_X + PLAYER_W > obs1_x) &&
-    (player_y < GROUND_Y) &&
-    (player_y + PLAYER_H > GROUND_Y-OBS_HEIGHT);
-
-assign c2 =
-    (PLAYER_X < obs2_x + OBS_WIDTH) &&
-    (PLAYER_X + PLAYER_W > obs2_x) &&
-    (player_y < GROUND_Y) &&
-    (player_y + PLAYER_H > GROUND_Y-OBS_HEIGHT);
-
-assign c3 =
-    (PLAYER_X < obs3_x + OBS_WIDTH) &&
-    (PLAYER_X + PLAYER_W > obs3_x) &&
-    (player_y < GROUND_Y) &&
-    (player_y + PLAYER_H > GROUND_Y-OBS_HEIGHT);
+//// logic//
 
 assign collision = c0 | c1 | c2 | c3;
 
